835f788c48bf7d4f12f227099ce193ba0456c235
Gina Häußge <gina@octoprint.org>
2026-02-17 14:50:23 +0100

    ci(e2e): some more user agent tests

--- tests/playwright/specs/user-agents.spec.js
+++ tests/playwright/specs/user-agents.spec.js
@@ -1,27 +1,70 @@
 const {test, expect} = require("../fixtures");
 
 const USER_AGENTS = [
-    {ua: "", browser_name: true, browser_version: true, os_name: true}, // default
+    {
+        ua: "",
+        browser_name: true,
+        browser_version: true,
+        os_name: true,
+        is_safari: false,
+        is_mac: false
+    }, // default
     {
         ua: "Mozilla/5.0 (X11; Linux x86_64; rv:147.0) Gecko/20100101 Firefox/147.0",
         browser_name: "Firefox",
         browser_version: "147.0",
-        os_name: "Linux"
+        os_name: "Linux",
+        is_safari: false,
+        is_mac: false
+    },
+    {
+        ua: "Mozilla/5.0 (Macintosh; Intel Mac OS X 15_7_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.0 Safari/605.1.15",
+        browser_name: "Safari",
+        browser_version: "26.0",
+        os_name: "Mac OS",
+        is_safari: true,
+        is_mac: true
+    },
+    {
+        ua: "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1",
+        browser_name: "Mobile Safari",
+        browser_version: "15.0",
+        os_name: "iOS",
+        is_safari: true,
+        is_mac: false
     },
     {
         ua: "PrusaSlicer/2.9.4 (Windows)",
         browser_name: "PrusaSlicer",
         browser_version: "2.9.4",
-        os_name: "Windows"
+        os_name: "Windows",
+        is_safari: false,
+        is_mac: false
     }, // see # 5235
     {
         ua: "PrusaSlicer/2.9.4 (Linux)",
         browser_name: "PrusaSlicer",
         browser_version: "2.9.4",
-        os_name: "Linux"
+        os_name: "Linux",
+        is_safari: false,
+        is_mac: false
     }, // see # 5235
-    {ua: "curl/1.2.3", browser_name: false, browser_version: false, os_name: false},
-    {ua: "something weird", browser_name: false, browser_version: false, os_name: false}
+    {
+        ua: "curl/1.2.3",
+        browser_name: false,
+        browser_version: false,
+        os_name: false,
+        is_safari: false,
+        is_mac: false
+    },
+    {
+        ua: "something weird",
+        browser_name: false,
+        browser_version: false,
+        os_name: false,
+        is_safari: false,
+        is_mac: false
+    }
 ];
 
 const checkStringValue = (expected, actual) => {
@@ -36,7 +79,7 @@ const checkStringValue = (expected, actual) => {
     }
 };
 
-USER_AGENTS.forEach(({ua, browser_name, browser_version, os_name}) => {
+USER_AGENTS.forEach(({ua, browser_name, browser_version, os_name, is_safari, is_mac}) => {
     test.describe(`User Agent: ${ua ? ua : "default"}`, () => {
         test.use({userAgent: ua});
 
@@ -62,11 +105,26 @@ USER_AGENTS.forEach(({ua, browser_name, browser_version, os_name}) => {
             const reportedBrowserInfo = await page.evaluate(() => {
                 return window.OctoPrint.coreui.browser;
             });
-            console.log("Browser:", reportedBrowserInfo);
+            //console.log("Browser:", reportedBrowserInfo);
 
             checkStringValue(browser_name, reportedBrowserInfo.browser_name);
             checkStringValue(browser_version, reportedBrowserInfo.browser_version);
             checkStringValue(os_name, reportedBrowserInfo.os_name);
+
+            expect(reportedBrowserInfo.safari).toBe(is_safari);
+            expect(reportedBrowserInfo.is_mac).toBe(is_mac);
+
+            if (is_safari) {
+                await expect(page.locator("html")).toHaveClass(/(^|\s)safari(\s|$)/);
+            } else {
+                await expect(page.locator("html")).not.toHaveClass(/(^|\s)safari(\s|$)/);
+            }
+
+            if (is_mac) {
+                await expect(page.locator("html")).toHaveClass(/(^|\s)macos(\s|$)/);
+            } else {
+                await expect(page.locator("html")).not.toHaveClass(/(^|\s)macos(\s|$)/);
+            }
         });
     });
 });
